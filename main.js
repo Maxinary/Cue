b0c658881b07deec00181eaeb6a89aca90114f48
Maximilian Victor Scribner <max.scribs@gmail.com>
2017-01-22 01:09:13 -0500

    Maybe mobile nice?

--- main.js
+++ main.js
@@ -27,10 +27,11 @@ function setWorldState(state){
   switch(state){
     case WorldStates.MainPage://draw
       $("#add").animate({"top":"-100%"}, 1000, "swing", null);
-      $("#time").animate({"top":"100%"}, 1000, "swing", null);
+      $("#mainPage").css({"overflow":"auto"});
       break;
     case WorldStates.Add:
       $("#add").animate({"top":"0"}, 1000, "swing", null);
+      $("#mainPage").css({"overflow":"hidden"});
       break;
   }
 }
